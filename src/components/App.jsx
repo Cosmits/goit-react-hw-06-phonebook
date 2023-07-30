82be01f48543574ae5b5dfaaf159eedd8e94adc7
Cosmits <salge.cos@gmail.com>
2023-07-30 22:29:29 +0200

    reduxjs/toolkit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,46 +4,33 @@ import Section from "./Section";
 import ContactForm from './ContactForm';
 import FilterInput from './FilterInput/FilterInput';
 import ContactsList from './ContactsList/ContactsList';
-// import {
-//   loadLocalStorage,
-//   saveLocalStorage
-//} from 'utils/localStorage';
-import { useDispatch, useSelector } from 'react-redux';
-import { delContacts, seFilter, setContacts } from 'redux/contactsReducer';
 
-// const defContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ]
+import { useDispatch, useSelector } from 'react-redux';
+import { setContactSlice, delContactSlice } from 'redux/contactsSlice';
+import { setFilterSlice } from 'redux/filterSlice';
+import { getContacts, getFilter } from 'redux/selectors';
 
 export default function App() {
 
-  // const [contacts, setContacts] = useState(loadLocalStorage() ?? defContacts);
-  // const [filter, setFilter] = useState('');
-
   const dispatch = useDispatch();
 
-  const contacts = useSelector((state) => state.contacts.contacts);
-  const filter = useSelector((state) => state.contacts.filter);
+  const { contacts } = useSelector(getContacts);
+  const { filter } = useSelector(getFilter);
 
   const addContact = data => {
     const newContact = { ...data, id: nanoid(), };
-    
-      contacts.some(({ name }) => name === data.name)
+
+    contacts.some(({ name }) => name === data.name)
       ? alert(`${data.name} is duplicate contact`)
-      : dispatch(setContacts(newContact))
-    //   //setContacts(prevContacts => [...prevContacts, newContact,]);
+      : dispatch(setContactSlice(newContact))
   };
 
   const delContact = userId => {
-    dispatch(delContacts(userId));
+    dispatch(delContactSlice(userId));
   };
 
   const handleChangeFilter = (event) => {
-    // setFilter(event.currentTarget.value.toLowerCase());
-    dispatch(seFilter(event.currentTarget.value.toLowerCase()))
+    dispatch(setFilterSlice(event.currentTarget.value.toLowerCase()))
   };
 
   const getFilterContacts = () => {
@@ -51,10 +38,6 @@ export default function App() {
       name.toLowerCase().includes(filter)
     );
   };
-
-  // useEffect(() => {
-  //   saveLocalStorage(contacts);
-  // }, [contacts])
 
   //================================================================
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,59 @@
-import { combineReducers, createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+// import { combineReducers, createStore } from 'redux';
+// import { devToolsEnhancer } from '@redux-devtools/extension';
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 
-import { contactsReducer } from './contactsReducer';
+// import { contactsReducer } from './contactsReducer';
 
-const rootReducer = combineReducers({
-  contacts: contactsReducer
+// const rootReducer = combineReducers({
+//   contacts: contactsReducer
+// });
+
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+// //  blacklist: ['contacts'],
+// }
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+// const enhancer = devToolsEnhancer();
+// export const store = createStore(persistedReducer, enhancer);
+// export const persistor = persistStore(store);
+
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['contacts'],
+};
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    filter: filterReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-const persistConfig = {
-  key: 'root',
-  storage,
-//  blacklist: ['contacts'], 
-}
-
-const persistedReducer = persistReducer(persistConfig, rootReducer)
-
-const enhancer = devToolsEnhancer();
-export const store = createStore(persistedReducer, enhancer);
 export const persistor = persistStore(store);
